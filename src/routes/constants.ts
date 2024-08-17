62932a26f4031e10160982dd4a2ecc16b6241812
Pavlo Chabanenko <chabanenkopp@gmail.com>
2024-08-17 19:07:50 +0200

    remove unnecessary routes

--- src/routes/constants.ts
+++ src/routes/constants.ts
@@ -1,12 +1,10 @@
 export enum Routes {
   Home = 'Home',
-  Dashboard = 'Dashboard',
   PostDetail = 'PostDetail',
 }
 
 export type RootDrawerParamList = {
   Home: undefined;
-  Dashboard: undefined;
   PostDetail: {
     post: {
       id: string;
